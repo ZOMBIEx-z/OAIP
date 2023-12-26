beecfefc8d5a30cce9284c95806bde8f3f2a0815
Tsimafei Melianiaka <65463236+ZOMBIEx-z@users.noreply.github.com>
2023-12-26 21:55:35 +0300

    add fiels of lw6

--- task2.c
+++ task2.c
@@ -1,72 +1,134 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+int min(int* arr, int size) {
+    if (size == 0) {
+        return -1; // Возвращаем -1, если массив пустой
+    } else {
+        int min_element = arr[0]; // Предполагаем, что первый элемент - минимальный
+        for (int i = 1; i < size; i++) {
+            if (arr[i] < min_element) {
+                min_element = arr[i]; // Если находим элемент меньше текущего минимального, обновляем его
+            }
+        }
+        return min_element;
+    }
+}
+
+void InsertionSort(int **arr, int size_x, int size_y)
+{
+    int* newElement;
+    int location;
+
+    for (int i = 1; i < size_x; i++)
+    {
+        newElement = arr[i];
+        location = i - 1;
+        while(location >= 0 && min(arr[location], size_y) > min(newElement, size_y))
+        {
+            arr[location+1] = arr[location];
+            location = location - 1;
+        }
+        arr[location+1] = newElement;
+    }
+}
+
+bool isNumber(const char* str) {
+    int i = 0;
+
+    // Учет знака числа
+    if (str[0] == '-' || str[0] == '+') {
+        i = 1;
+    }
+
+    // Проверка каждого символа на цифру
+    for (; i < strlen(str); i++) {
+        if (str[i] < '0' || str[i] > '9') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int getIntegerFromUseri(int i, int j) {
+    char input[100];
+
+    printf("Enter an integer value of matrix[%d][%d]: ", i, j);
+    scanf("%s", input);
+    while (!isNumber(input)) {
+        printf("Error! Enter an integer value of matrix[%d][%d]: ", i, j);
+        scanf("%s", input);
+
+    }
+
+    return atoi(input);
+}
+
+int getIntegerFromUser() {
+    char input[100];
+
+    printf("Enter a size of array: ");
+    scanf("%s", input);
+    while (!isNumber(input) || (atoi(input) < 0)) {
+        printf("Error! Enter a size of array: ");
+        scanf("%s", input);
+    }
+
+    return atoi(input);
+}
 
 int main() {
 
-    int size;
+    printf("How do you want to fill array (r-random, other-from keyboard): ");
+    char line[100];
+    fgets(line, sizeof(line), stdin);
 
-    printf("size of array: ");
-    scanf("%d", &size);
+    int size_x = getIntegerFromUser();
+    int size_y = getIntegerFromUser();
 
-    int **array = (int **) malloc(size * sizeof(int *));
 
-    int k;
+    int** list;
+    do {
+        list = (int**) malloc(sizeof(int*) * size_x);
+    } while (list == NULL);
 
-    for (int i = 0; i < size; i += 1) {
-        int j = 0;
-        while (true) {
 
-            array[i] = (int *) realloc(array[i], (j + 1) * sizeof(int));
-            printf("Enter the value of array[%d][%d]: ", i, j);
-            scanf("%d", &k);
+    for (int i = 0; i < size_x; i += 1) {
+        do {
+            list[i] = (int*) malloc(sizeof(int) * size_y);
+        } while (list[i] == NULL);
+    }
 
-            array[i][j] = k;
-            j += 1;
-            if (k == 100) {
-                break;
+    if (line[0] == 'r') {
+        for (int i = 0; i < size_x; i++) {
+            for (int j = 0; j < size_y; j += 1) {
+                list[i][j] = -10000 + rand() % 20000;
             }
-
         }
-    }
-
-    for (int i = 0; i < size; i += 1) {
-        int j = 0;
-        int p;
-        for (p = 0; array[i][p] != 100; p += 1);
-        p += 1;
-        while (array[i][j] != 100) {
-
-            if (array[i][j] < 0) {
-                for (int k = j; k < p - 1; k += 1) {
-                    array[i][k] = array[i][k + 1];
-                }
-                p -= 1;
-                array[i] = (int *) realloc(array[i], p * sizeof(int));
-                j -= 1;
+    } else {
+        for (int i = 0; i < size_x; i++) {
+            for (int j = 0; j < size_y; j += 1) {
+                list[i][j] = getIntegerFromUseri(i, j);
             }
-
-            j +=1;
         }
     }
 
-    for (int i = 0; i < size; i += 1) {
-        for (int j = 0; array[i][j] != 100; j += 1) {
+    InsertionSort(list, size_x, size_y);
 
-            printf("%d ", array[i][j]);
-
-
+    for (int i = 0; i < size_x; i += 1) {
+        for (int j = 0; j < size_y; j += 1) {
+            printf("%d ", list[i][j]);
         }
-        printf("100\n");
+        printf("\n");
     }
 
-    for (int i = 0; i < size; i += 1) {
-        free(array[i]);
+    for (int i = 0; i < size_x; i += 1) {
+        free(list[i]);
     }
-
-
-    free(array);
+    free(list);
 
     return 0;
 }
-
