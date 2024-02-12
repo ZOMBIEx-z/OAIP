eeeca04cce972f032b2e73e7011c78c1878cb6d3
Tsimafei Melianiaka <65463236+ZOMBIEx-z@users.noreply.github.com>
2024-02-12 20:06:07 +0300

    update LW_6 task1

--- task1.c
+++ task1.c
@@ -3,22 +3,69 @@
 #include <stdbool.h>
 #include <string.h>
 
-void shell_sort(int *array, int size) {
-    for (int s = size / 2; s > 0; s /= 2) {
-        for (int i = s; i < size; ++i) {
-            for (int j = i - s; j >= 0 && array[j] > array[j + s]; j -= s) {
-                if (array[j] > 0 && array[j + s] > 0 && array[j] % 2 == 0 && array[j + s] % 2 == 0) {
-                    int temp = array[j];
-                    array[j] = array[j + s];
-                    array[j + s] = temp;
-                }
+void shellSort(int *arr, int size_arr) {
 
-            }
+    int tmp = size_arr;
+    for (int i = 0; i < size_arr; i += 1) {
+        if (arr[i] > 0 && arr[i] % 2 == 0) {
+            arr[tmp] = arr[i];
+            tmp += 1;
         }
     }
+/*
+    for (int i = 0; i < size_arr * 2; i += 1) {
+        printf("%d ", arr[i]);
+
+    }
+    printf("\n");
+*/
+
+    int start = size_arr;
+    int end = tmp - 1;
+
+    int size = end - start + 1;
+    int gap = 1;
+
+    while (gap < size / 3) {
+        gap = gap * 3 + 1;
+    }
+
+    while (gap > 0) {
+        for (int i = start + gap; i <= end; ++i) {
+            int temp = arr[i];
+            int j = i;
+
+            while (j >= start + gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+
+            arr[j] = temp;
+        }
+
+        gap /= 3;
+    }
+
+    /*
+    for (int i = 0; i < size_arr * 2; i += 1) {
+        printf("%d ", arr[i]);
+
+    }
+    printf("\n");
+*/
+
+    tmp = size_arr;
+    for (int i = 0; i < size_arr; i += 1) {
+        if (arr[i] > 0 && arr[i] % 2 == 0) {
+            arr[i] = arr[tmp];
+            tmp += 1;
+        }
+    }
+
 }
 
-bool isNumber(const char* str) {
+
+bool isNumber(const char *str) {
     int i = 0;
 
     // Учет знака числа
@@ -71,26 +118,32 @@ int main() {
 
     int size_x = getIntegerFromUser();
 
-    int* list;
+    int *list;
     do {
-        list = (int*) malloc(sizeof(int) * size_x);
+        list = (int *) malloc(sizeof(int) * size_x * 2);
     } while (list == NULL);
 
 
     if (line[0] == 'r') {
         for (int i = 0; i < size_x; i++) {
-                list[i] = -10000 + rand() % 20000;
+            list[i] = -10 + rand() % 20;
         }
     } else {
         for (int i = 0; i < size_x; i++) {
-                list[i] = getIntegerFromUseri(i);
+            list[i] = getIntegerFromUseri(i);
         }
     }
 
-    shell_sort(list, size_x);
+    for (int i = 0; i < size_x; i += 1) {
+        printf("%d ", list[i]);
+
+    }
+    printf("\n");
+
+    shellSort(list, size_x);
 
     for (int i = 0; i < size_x; i += 1) {
-            printf("%d ", list[i]);
+        printf("%d ", list[i]);
 
     }
 
